75ef37c0d9b6601e001f8fa07dd05318ffca2e13
DavidStewartLDN <davidstewart.work@gmail.com>
2020-07-14 13:14:22 +0100

    first commit, able to recieve data

---
+++ python-finance.py
@@ -0,0 +1,16 @@
+import datetime as dt
+import matplotlib.pyplot as pyplot
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+
+# choose styling for plot, many other optons available
+style.use('ggplot')
+
+# start and end date of information
+start = dt.datetime(2000,1,1)
+end = dt.datetime(2016,12,31)
+
+# data frame
+df = web.DataReader('TSLA', 'yahoo', start, end)
+print(df.head())
