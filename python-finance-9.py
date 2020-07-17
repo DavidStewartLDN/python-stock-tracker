dde044d82fd6c3c818c4d9851667e8234bd13eba
DavidStewartLDN <davidstewart.work@gmail.com>
2020-07-17 09:45:42 +0100

    updated comments and changed to 5 day (market closed on weekends)

--- python-finance-9.py
+++ python-finance-9.py
@@ -5,13 +5,15 @@ import pickle
 
 # each model will per company but compared against all
 def process_data_for_labels(ticker):
-  hm_days = 7
+  hm_days = 5
   df = pd.read_csv('sp500_joined_closes.csv', index_col=0)
   tickers = df.columns.values.tolist()
   df.fillna(0, inplace=True)
 
   for i in range(1, hm_days+1):
     # new minus the old
+    # this shifts the column ticker_{}d up by the value of i so that you compare the current
+    # value to {} days from now (future price)
     df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
   
   df.fillna(0, inplace=True)
